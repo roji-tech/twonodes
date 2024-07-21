123066c8296d8aa692414d6e8205c8918314930a
roji-tech <rojitech9@gmail.com>
2024-07-21 23:32:27 +0100

    _

--- components/HomeComponents.tsx
+++ components/HomeComponents.tsx
@@ -400,27 +400,47 @@ export const OurGallery = () => {
 };
 
 export const FrequentlyAskedQuestions = () => {
+  const plusSvg = (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2 10H18M10.0001 2V18"
+        stroke="#141414"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+
   return (
     <div className="mywrapper w-full min-h-[450px] py-[75px] bg-white flex-col justify-start items-center inline-flex">
-      <div className="min-h-[300px] flex-col justify-start items-center gap-[60px] flex">
+      <div className="w-full min-h-[300px] flex-col justify-start items-center gap-[60px] flex">
         <div className="text-center text-neutral-900 text-5xl font-semibold capitalize">
           Frequently Asked Questions
         </div>
-        <div className="flex-col justify-start items-start gap-[18px] flex">
-          <div className="flex-col justify-start items-start flex">
+
+        <div className="w-full flex-col justify-start items-start gap-[18px] flex">
+          <div className="cursor-pointer w-full flex-col justify-start items-start flex">
             <div className="w-full px-[30px] py-6 bg-gray-50 justify-between items-center inline-flex">
               <div className="text-neutral-900 text-2xl font-bold leading-[34px]">
                 What do we do at TwoNode Technologies?
               </div>
-              <div className="w-5 h-5 relative" />
+              {plusSvg}
             </div>
           </div>
-          <div className="flex-col justify-start items-start flex">
+
+          <div className="cursor-pointer w-full flex-col justify-start items-start flex">
             <div className="w-full px-[30px] py-6 bg-gray-50 justify-between items-center inline-flex">
               <div className="text-neutral-900 text-2xl font-bold leading-[34px]">
                 On what platform is your solutions built?
               </div>
-              <div className="w-5 h-5 relative" />
+              {plusSvg}
             </div>
           </div>
         </div>
