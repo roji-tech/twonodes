00bd43cefa0388b308949b8ca55c29cb023cc130
roji-tech <rojitech9@gmail.com>
2024-07-29 12:13:13 +0100

    __

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,33 @@ export default function RootLayout({
           content="Copyright © 2024 TwoNode Technologies"
         />
 
+        {/* ICONS */}
+        {/* <!-- Favicon  --> */}
+        {/* <link rel="icon" href="favicon.jpg" /> */}
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff"></meta>
+        {/*  */}
+
         {/* <!-- OG Meta Tags to improve the way the post looks when you share the page on LinkedIn, Facebook, Google+ --> */}
         <meta property="og:site_name" content="TwoNode Technologies" />
         {/* <!-- website name --> */}
@@ -93,9 +120,6 @@ export default function RootLayout({
           rel="stylesheet"
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-
-        {/* <!-- Favicon  --> */}
-        <link rel="icon" href="favicon.jpg" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
