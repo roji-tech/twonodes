06a0f70594baeb1a805d3e38ca17f40169682a9e
roji-tech <rojitech9@gmail.com>
2024-07-29 09:54:46 +0100

    __

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/:path*", // Apply to all routes
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "index, follow", // Customize the value as needed
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
