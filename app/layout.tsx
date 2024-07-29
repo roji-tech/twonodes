9e5c501a5555f7a0bedc0cb7562852993231fd56
roji-tech <rojitech9@gmail.com>
2024-07-29 04:29:59 +0100

    __

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import Link from "next/link";
 // const inter = Inter({ subsets: ["latin"] });
 // inter.className
 export const metadata: Metadata = {
-  title: "TWONODE",
-  description: "TWONODE TECH",
+  title: "TwoNode",
+  description: "TwoNode TECH",
 };
 
 export default function RootLayout({
@@ -28,7 +28,7 @@ export default function RootLayout({
         {/* <!-- SEO Meta Tags --> */}
         <meta
           name="description"
-          content="PluttrApp helps you manage your finances with ease. Track your expenses, set budgets, and achieve your financial goals."
+          content="TwoNode is an The Platform for Industry Based GIS Solutions"
         />
         <meta name="author" content="TwoNode" />
 
@@ -42,36 +42,33 @@ export default function RootLayout({
         {/* <!-- website link --> */}
         <meta
           property="og:title"
-          content="PluttrApp - Track Your Finances Effortlessly"
+          content="TwoNode - The Platform for Industry Based GIS Solutions"
         />
         {/* <!-- title shown in the actual shared post --> */}
         <meta
           property="og:description"
-          content="PluttrApp helps you manage your finances with ease. Track your expenses, set budgets, and achieve your financial goals."
+          content="TwoNode - The Platform for Industry Based GIS Solutions"
         />
         {/* <!-- description shown in the actual shared post --> */}
         <meta
           property="og:image"
-          content="https://www.twonodetechnologies.com/public/logo.png."
+          content="https://www.twonodetechnologies.com/favicon.jpg."
         />
         {/* <!-- image link, make sure it's jpg --> */}
-        <meta
-          property="og:url"
-          content="https://www.twonodetechnologies.com"
-        />
+        <meta property="og:url" content="https://www.twonodetechnologies.com" />
         {/* <!-- where do you want your post to link to --> */}
         <meta property="og:type" content="article" />
 
         {/* <!-- Twitter Card Meta Tags --> */}
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@PluttrApp" />
+        <meta name="twitter:site" content="@TwoNode" />
         <meta
           name="twitter:title"
-          content="PluttrApp - Track Your Finances Effortlessly"
+          content="TwoNode - The Platform for Industry Based GIS Solutions"
         />
         <meta
           name="twitter:description"
-          content="PluttrApp helps you manage your finances with ease. Track your expenses, set budgets, and achieve your financial goals."
+          content="TwoNode is an The Platform for Industry Based GIS Solutions"
         />
         <meta
           name="twitter:image"
@@ -79,7 +76,7 @@ export default function RootLayout({
         />
 
         {/* <!-- Website Title --> */}
-        <title>PluttrApp - Track Your Finances Effortlessly</title>
+        <title>TwoNode - The Platform for Industry Based GIS Solutions</title>
 
         {/* <!-- Styles --> */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -93,24 +90,9 @@ export default function RootLayout({
           rel="stylesheet"
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin=""
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-          rel="stylesheet"
-        />
-
-        <link href="css/bootstrap.css" rel="stylesheet" />
-        <link href="css/fontawesome-all.css" rel="stylesheet" />
-        <link href="css/swiper.css" rel="stylesheet" />
-        <link href="css/magnific-popup.css" rel="stylesheet" />
-        <link href="css/styles.css" rel="stylesheet" />
 
         {/* <!-- Favicon  --> */}
-        <link rel="icon" href="images/favicon.png" />
+        <link rel="icon" href="favicon.jpg" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
