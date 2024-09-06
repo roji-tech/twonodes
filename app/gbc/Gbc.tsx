7a0b212ab640d9887b69321185c1347c1a0f815c
roji-tech <rojitech9@gmail.com>
2024-09-06 18:42:18 +0100

    _

--- app/gbc/Gbc.tsx
+++ app/gbc/Gbc.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import Link from "next/link";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 const StarSvg = (
   <svg
@@ -8,7 +14,7 @@ const StarSvg = (
     height="46"
     viewBox="0 0 45 46"
     fill="none"
-    className="cursor-pointer"
+    className="cursor-pointer w-8 lg:w-[45px]"
   >
     <rect y="0.5" width="45" height="45" rx="22.5" fill="#001F3F" />
     <path
@@ -20,7 +26,7 @@ const StarSvg = (
 
 const ShareSvg = (
   <svg
-    className="cursor-pointer"
+    className="cursor-pointer w-8 lg:w-[45px]"
     xmlns="http://www.w3.org/2000/svg"
     width="45"
     height="46"
@@ -170,59 +176,62 @@ export const Gbc = () => {
   return (
     <div className="mywrapper py-[55px] bg-white justify-start items-center gap-2.5 inline-flex">
       <div className="w-full flex flex-col justify-start items-start gap-10">
-        <div className="self-stretch min-h-[380px] flex-col justify-start items-start gap-[90px] flex">
-          <div className="flex justify-between items-center gap-[20px] w-full">
-            <h2 className="text-[#001f3f] text-[40px] font-extrabold font-['Bricolage Grotesque']">
+        <div className="self-stretch min-h-[380px] flex-col justify-start items-start lg:gap-[90px] gap-[30px] flex">
+          <div className="flex justify-between items-center flex-wrap lg:gap-[20px] gap-2 w-full">
+            <h2 className="text-[#001f3f] lg:text-[40px] text-[25px] font-extrabold font-['Bricolage Grotesque']">
               Geospatial Builders Course 1.0
             </h2>
-            <div className="justify-center items-center gap-[15px] flex w-max">
+            <div className="justify-end items-center gap-[15px] flex sm:w-max w-full">
               {StarSvg}
               {ShareSvg}
             </div>
           </div>
 
           <div className="self-stretch justify-between items-start flex flex-wrap">
-            <div className="w-[430px] flex-col justify-start items-start gap-5 inline-flex">
-              <h4 className="text-[#001f3f] text-[32px] font-bold font-['Bricolage Grotesque']">
+            <div className="w-full flex-col justify-start items-start gap-5 inline-flex">
+              <h4 className="text-[#001f3f] lg:text-[32px] text-[22px] font-bold font-['Bricolage Grotesque']">
                 Date and Time
               </h4>
               <div className="self-stretch min-h-[177px] flex-col justify-start items-start gap-[15px] flex">
                 <div className="ml-[10px] p-2.5 justify-center items-center gap-5 flex">
                   {CalendarSvg}
-                  <span className="text-[#001f3f] text-2xl font-semibold font-['Bricolage Grotesque']">
+                  <span className="text-[#001f3f] text-xl lg:text-2xl font-semibold font-['Bricolage Grotesque']">
                     Saturday, November 23rd, 2024
                   </span>
                 </div>
                 <div className="ml-[10px] p-2.5 justify-center items-center gap-5 flex">
                   {ClockSvg}
-                  <span className="text-[#001f3f] text-2xl font-semibold font-['Bricolage Grotesque']">
+                  <span className="text-[#001f3f] text-xl lg:text-2xl font-semibold font-['Bricolage Grotesque']">
                     9 AM Prompt
                   </span>
                 </div>
                 <div className="ml-[10px] p-2.5 justify-center items-center gap-5 flex">
                   {PlusSvg}
-                  <span className="text-[#00bfff] text-2xl font-medium font-['Bricolage Grotesque']">
+                  <span className="text-[#00bfff] text-xl lg:text-2xl font-medium font-['Bricolage Grotesque']">
                     Add to Calendar
                   </span>
                 </div>
               </div>
             </div>
             <div className="flex-col justify-start items-end gap-[50px] inline-flex">
-              <div className="w-[324px] min-h-[84px] p-2.5 bg-[#00bfff] rounded-[5px] flex-col justify-center items-center gap-2.5 flex">
-                <Link href={"gbc-frame"} className="cursor-pointer justify-center items-center gap-[15px] inline-flex">
+              <div className="w-[324px] mAX-h-[84px] p-2.5 bg-[#00bfff] rounded-[5px] flex-col justify-center items-center gap-2.5 flex">
+                <Link
+                  href={"gbc-frame"}
+                  className="cursor-pointer justify-center items-center gap-[15px] inline-flex"
+                >
                   {BuyTicketSvg}
-                  <span className="text-white text-[32px] font-semibold font-['Bricolage Grotesque']">
+                  <span className="text-white lg:text-[32px] text-[22px] font-semibold font-['Bricolage Grotesque']">
                     Buy Tickets
                   </span>
                 </Link>
               </div>
               <div className="flex-col justify-start items-start gap-[26px] flex">
-                <h4 className="text-[#001f3f] text-[32px] font-bold font-['Bricolage Grotesque']">
+                <h4 className="text-[#001f3f] lg:text-[32px] text-[22px] font-bold font-['Bricolage Grotesque']">
                   Ticket Information
                 </h4>
                 <div className="justify-start items-center gap-2.5 inline-flex">
                   {StandardTicketSvg}
-                  <div className="text-[#001f3f] text-2xl font-semibold font-['Bricolage Grotesque']">
+                  <div className="text-[#001f3f] text-xl lg:text-2xl font-semibold font-['Bricolage Grotesque']">
                     Standard Ticket: 70k each
                   </div>
                 </div>
@@ -233,13 +242,13 @@ export const Gbc = () => {
 
         <div className="flex-col justify-start items-start gap-[25px] flex">
           <div className="flex-col justify-start items-start gap-5 flex">
-            <h3 className="text-[#001f3f] text-[32px] font-bold font-['Bricolage Grotesque']">
+            <h3 className="text-[#001f3f] lg:text-[32px] text-[22px] font-bold font-['Bricolage Grotesque']">
               Location
             </h3>
             <div className="flex-col justify-start items-start gap-[15px] flex">
               <div className="p-2.5 justify-center items-center gap-5 inline-flex">
                 {LocationSvg}
-                <p className="max-w-[537px] text-[#001f3f] text-2xl font-semibold font-['Bricolage Grotesque']">
+                <p className="max-w-[537px] text-[#001f3f] text-xl lg:text-2xl font-semibold font-['Bricolage Grotesque']">
                   Lagos Marriott Hotel Ikeja - 122 Joel Ogunnaike St, Ikeja GRA,
                   Lagos 100271, Lagos.
                 </p>
@@ -253,7 +262,7 @@ export const Gbc = () => {
         </div>
 
         <div className="flex-col justify-start items-start gap-5 flex">
-          <h3 className="text-[#001f3f] text-[32px] font-bold font-['Bricolage Grotesque']">
+          <h3 className="text-[#001f3f] lg:text-[32px] text-[22px] font-bold font-['Bricolage Grotesque']">
             HOW TO PAY FOR GBC 1.0
           </h3>
 
@@ -288,7 +297,10 @@ export const Gbc = () => {
               </span>
               <span className="flex items-center gap-2">
                 <div className="w-2 h-1 rounded-3xl bg-[#001f3f] ml-2" />
-                Email : - events@twonodetechnologies.com
+                Email : -
+                <span className="sm:text-xl text-xs">
+                  events@twonodetechnologies.com
+                </span>
               </span>
             </div>
           </div>
@@ -321,19 +333,83 @@ export const Gbc = () => {
           </div>
         </div>
 
-        <div className="w-full h-[78px] px-[34px] py-5 bg-[#f8fafb] rounded-[10px] flex-col justify-center items-center gap-2.5 flex">
-          <div className="h-[38px] flex-col justify-start items-start gap-5 flex w-full">
-            <div className="px-[26px] justify-between items-center gap-[20px] flex w-full">
-              <h3 className="text-[#001f3f] text-[32px] font-bold font-['Bricolage Grotesque']">
-                Event Description
-              </h3>
-              <div className="w-8 h-8 relative origin-top-left rotate-90" />
+        <div className="w-full min-h-[78px] px-[34px] py-5 bg-[#f8fafb] rounded-[10px] flex-col justify-center items-center gap-2.5 flex">
+          <div className="min-h-[38px] flex-col justify-start items-start gap-5 flex w-full">
+            <div className="justify-between items-center gap-[20px] flex w-full">
+              <Accordion
+                className="w-full flex-col justify-start items-start gap-[18px] flex"
+                type="single"
+                collapsible
+              >
+                <AccordionItem className="w-full" value="item-1">
+                  <AccordionTrigger className="w-full">
+                    <div className="cursor-pointer w-full flex-col justify-start items-start flex">
+                      <div className="w-full bg-gray-50 justify-between items-center inline-flex">
+                        <h2 className="text-[#001f3f] lg:text-[32px] text-[22px] font-bold font-['Bricolage Grotesque']">
+                          Event Description
+                        </h2>
+                      </div>
+                    </div>
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    <div className="">
+                      <h3 className="text-[#001f3f] font-bold text-2xl">A wake-up call!!!</h3>
+                      <span className="text-[#001f3f] text-xl">
+                        Nowadays we are potentially two persons or a person away
+                        from reaching Bill Gates, the President or any other
+                        high network individual. That's because all our
+                        interactions, transactions and activities have become
+                        virtual, even products and services. For example, in
+                        government, certificates are issued in soft copies,
+                        electronic surveys, electronic CofO and even e-receipts.
+                        This has made us take a closer look into our
+                        professional environment and we discover that the
+                        reality is no different. The need for remote access
+                        services to effectively convey our services and products
+                        from the physical platform to various digital platforms
+                        has become essential in today's digital age.
+                      </span>
+                      <br />
+                      <br />
+                      <span className="text-[#001f3f] text-xl">
+                        The Current trends, development and policies around us
+                        call for a need to be seen as a versatile geospatial
+                        specialist rather than being confined to the public's
+                        view of land surveyors, it is imperative to break free
+                        from this narrow viewpoint. Embracing a holistic
+                        approach to geospatial knowledge and skills is essential
+                        for professionals to thrive in today's rapidly changing
+                        and dynamic environment.
+                      </span>
+                      <br />
+                      <br />
+                      <h3 className="text-[#001f3f] font-bold text-2xl">GIS as our ally!</h3>
+
+                      <span className="text-[#001f3f] text-xl">
+                        Throughout history, there has been a lack of proper
+                        synergy between Surveying and GIS. Rather, we usually
+                        hear things like application of GIS in transportation,
+                        application of GIS in land administration and so on. So
+                        we are asking, can GIS truly serve as a valuable ally to
+                        surveyors? Can GIS be what we call a "survey assistant?"
+                        that is enhance all our activities from field to office
+                        planning, field work execution, plan production, plan
+                        management or even extend our services as stated earlier
+                        to remote access? The answer is YES. Find out on
+                        November 23rd, 2024, as our first edition of Geospatial
+                        Builders Course begins with the theme Demystifying GIS
+                        for Surveyors.
+                      </span>
+                    </div>
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
             </div>
           </div>
         </div>
 
         <div className="flex-col justify-start items-start gap-5 flex w-full">
-          <h3 className="text-[#001f3f] text-[32px] font-bold font-['Bricolage Grotesque']">
+          <h3 className="text-[#001f3f] lg:text-[32px] text-[22px] font-bold font-['Bricolage Grotesque']">
             Tags
           </h3>
 
